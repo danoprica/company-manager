8ad733f490975267ff2147a7c4b1794907b92d95
danoprica <danutzoprica@yahoo.com>
2023-11-01 21:00:38 +0200

    Added employees select input in editProject blade

--- resources/views/projects/editProject.blade.php
+++ resources/views/projects/editProject.blade.php
@@ -9,8 +9,17 @@
             @csrf
             @method('put')
             <div>
-                <label>Name: </label>
+                <label class="pb-3">Name: </label>
                 <input value="{{$project->name}}" type="text" name="name"/>
+                <h5>Assign employees:</h5>
+                @foreach($employees as $employee)
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkbox-employee-{{$employee['id']}}" {{$employee->project_id == $project->id ? 'checked' : '' }}>
+                        <label class="form-check-label" for= "checkbox-employee-{{$employee['id']}}">
+                            {{$employee['name']}}
+                        </label>
+                    </div>
+                @endforeach
             </div>
             <br>
             <div>
